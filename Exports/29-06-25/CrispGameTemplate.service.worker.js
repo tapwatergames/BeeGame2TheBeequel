13c5705817339c7cd500ca1c60a425a072926159
Manny-OnGit <manny.fluss@gmail.com>
2025-06-29 17:38:53 -0700

    attempot2

--- Exports/29-06-25/CrispGameTemplate.service.worker.js
+++ Exports/29-06-25/CrispGameTemplate.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751243495|3656406506';
+const CACHE_VERSION = '1751243924|4085378450';
 /** @type {string} */
 const CACHE_PREFIX = 'BeeGame2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
